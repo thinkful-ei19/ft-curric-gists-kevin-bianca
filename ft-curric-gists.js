8d2f83217b56d7ded42febd5ff08bdf693ee637c
Kevin Murphy <midimurphdesigns@gmail.com>
2018-02-19 13:47:33 -0700

    added try catch

--- ft-curric-gists.js
+++ ft-curric-gists.js
@@ -5,6 +5,18 @@ function yearOfBirth(age) {
 
 function whoAmI(name, age) {
   let yob = yearOfBirth(age);
+  try {
+    if (age <= 0) {
+      throw new Error('must be at least 1 years old.');
+    }
+
+    if (typeof name !== 'string') {
+      throw new Error('must enter a valid name.')
+    }
+  } catch(e) {
+    console.error(e);
+  }
+
   console.log(`my name is ${name} and I\'m ${age} years old.`);
   console.log('I was born in ' + yob + '.');
 }
